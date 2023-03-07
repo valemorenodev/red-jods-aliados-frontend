3b5b163f498da9474f55030fe0443b60564b0d33
dianamorenodev <valentinamoreno2002@gmail.com>
2023-03-07 10:29:55 -0500

    media querys y formulario

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,30 @@
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
+import { Container, Row, Col } from 'react-bootstrap';
 import logo from '../../assets/images/logo.png'
+import { useForm } from "react-hook-form";
 import './Login.css'
 
 function Login() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(watch("example"));
   return (
     <>
       <Container id='container-login' className='d-flex justify-content-center align-items-center'>
-        <Form className='d-flex flex-column'>
-          <img className='logo' src={logo} alt="" />
-        </Form>
+        <Row className='d-flex justify-content-center align-items-center'>
+          <Col className='d-flex flex-column' >
+            <img className='logo' src={logo} alt="" />
+          </Col>
+        </Row>
+        <Row className='d-flex justify-content-center align-items-center'>
+          <Col>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input defaultValue="test" {...register("example")} />
+              <input {...register("exampleRequired", { required: true })} />
+              {errors.exampleRequired && <span>This field is required</span>}
+              <input type="submit" />
+            </form>
+          </Col>
+        </Row>
       </Container>
     </>
   )
