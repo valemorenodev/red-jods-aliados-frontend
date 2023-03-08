769182d296ec5a678eded8b646ae6422ca36c56e
dianamorenodev <valentinamoreno2002@gmail.com>
2023-03-08 09:55:23 -0500

    Error en login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,10 +2,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 import logo from '../../assets/images/logo.png'
 import { useForm } from "react-hook-form";
 import './Login.css'
-import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
-  const navigate = useNavigate();
+const Login = (props) => {
 
   const submitHandler = e => {
     e.preventDefault()
@@ -37,29 +35,30 @@ const Login = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     console.log(watch("example"));
-    return (
-      <>
-        <Container id='container-login' className='d-flex justify-content-around align-items-center'>
-          <Row className='d-flex justify-content-center align-items-center'>
-            <Col className='d-flex flex-column' >
-              <img className='logo' src={logo} alt="" />
-            </Col>
-          </Row>
-          <Row className='d-flex justify-content-center align-items-center'>
-            <Col>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="email">Nombre de usuario</label>
-                <input name="email" placeholder='example@email.com' {...register("example")} />
-                <label htmlFor="password">Constraseña</label>
-                <input name='password' type={'password'} placeholder={'**********'} {...register("exampleRequired", { required: true })} />
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input type="submit" id='sendButton' />
-              </form>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    )
   }
+  return (
+    <>
+      <Container id='container-login' className='d-flex justify-content-around align-items-center'>
+        <Row className='d-flex justify-content-center align-items-center'>
+          <Col className='d-flex flex-column' >
+            <img className='logo' src={logo} alt="" />
+          </Col>
+        </Row>
+        <Row className='d-flex justify-content-center align-items-center'>
+          <Col>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="email">Nombre de usuario</label>
+              <input name="email" value="email" placeholder='example@email.com' {...register("example")} />
+              <label htmlFor="password">Constraseña</label>
+              <input name='password' value="password" type={'password'} placeholder={'**********'} {...register("exampleRequired", { required: true })} />
+              {errors.exampleRequired && <span>This field is required</span>}
+              <input type="submit" id='sendButton' />
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
 
-  export default Login
+export default Login
