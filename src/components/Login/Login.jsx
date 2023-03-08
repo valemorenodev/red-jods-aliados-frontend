02cc9775ca0e964b66560d5ccdf934e473f53fd5
dianamorenodev <valentinamoreno2002@gmail.com>
2023-03-08 09:40:09 -0500

    Logica de validacion temporal

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,34 +2,64 @@ import { Container, Row, Col } from 'react-bootstrap';
 import logo from '../../assets/images/logo.png'
 import { useForm } from "react-hook-form";
 import './Login.css'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(watch("example"));
-  return (
-    <>
-      <Container id='container-login' className='d-flex justify-content-around align-items-center'>
-        <Row className='d-flex justify-content-center align-items-center'>
-          <Col className='d-flex flex-column' >
-            <img className='logo' src={logo} alt="" />
-          </Col>
-        </Row>
-        <Row className='d-flex justify-content-center align-items-center'>
-          <Col>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <label htmlFor="name">Nombre de usuario</label>
-              <input name="name" placeholder='example@email.com' {...register("example")} />
-              <label htmlFor="password">Constraseña</label>
-              <input name='password' type={'password'} placeholder={'**********'} {...register("exampleRequired", { required: true })} />
-              {errors.exampleRequired && <span>This field is required</span>}
-              <input type="submit" id='sendButton' />
-            </form>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  )
-}
+  const navigate = useNavigate();
 
-export default Login
+  const submitHandler = e => {
+    e.preventDefault()
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+
+
+    if (email === '' || password === '') {
+      alert("Los campos  no pueden estar vacíos")
+      return;
+    }
+
+    if (email !== '' && !regexEmail.test(email)) {
+      alert("Debes escribir una dirección de correo electrónico válida")
+      return;
+    }
+
+    if (email !== 'email@mail.com' || password !== 'password') {
+      alert("Credenciales inválidas")
+      return;
+    } else {
+      props.addToken(true)
+      navigate('/cripto')
+    }
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(watch("example"));
+    return (
+      <>
+        <Container id='container-login' className='d-flex justify-content-around align-items-center'>
+          <Row className='d-flex justify-content-center align-items-center'>
+            <Col className='d-flex flex-column' >
+              <img className='logo' src={logo} alt="" />
+            </Col>
+          </Row>
+          <Row className='d-flex justify-content-center align-items-center'>
+            <Col>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email">Nombre de usuario</label>
+                <input name="email" placeholder='example@email.com' {...register("example")} />
+                <label htmlFor="password">Constraseña</label>
+                <input name='password' type={'password'} placeholder={'**********'} {...register("exampleRequired", { required: true })} />
+                {errors.exampleRequired && <span>This field is required</span>}
+                <input type="submit" id='sendButton' />
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    )
+  }
+
+  export default Login
