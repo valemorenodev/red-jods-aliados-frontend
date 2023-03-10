12471b0e95596052087e43879e72abfcbda02cd5
Gerald <aligerald.cac7@gmail.com>
2023-03-10 15:12:39 -0500

    Create structure component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import RouterApp from './RouterApp'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <RouterApp />
+      <Header />
+      <Footer />
     </div>
   )
 }
