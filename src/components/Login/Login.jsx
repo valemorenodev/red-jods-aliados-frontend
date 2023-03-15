577b5096a33914285fc99ad8e2a570d26bb94e5c
dianamorenodev <valentinamoreno2002@gmail.com>
2023-03-15 10:07:34 -0500

    img import

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
 import logo from '../../assets/images/logo.png';
-import wheel from '../../assets/images/Recurso 4@4x.png'
+import wheel from '../../assets/images/Recurso4@4x.png'
 import style from './Login.module.css'
 import Swal from 'sweetalert2';
 import axiosInstance from '../../apis/index';
@@ -35,7 +35,7 @@ const Login = (props) => {
     if (email === 'email@mail.com' || pass === 'pass') {
       alert("Credenciales inválidas");
       return;
-    } 
+    }
     // else {
     //   props.addToken(true);
     //   Navigate('/cripto');
@@ -43,13 +43,13 @@ const Login = (props) => {
 
     try {
       const response = await axiosInstance.post('/auth/login', { email, pass });
-      navigate('/AlliesList'); 
+      navigate('/AlliesList');
     } catch (error) {
       alert(error);
     }
   };
 
- 
+
 
   return (
     <>
@@ -62,11 +62,11 @@ const Login = (props) => {
           </Row>
           <Row className='d-flex justify-content-center align-items-center'>
             <Col>
-            <form className={style.forms} onSubmit={handleSubmit(submitHandler)}>
+              <form className={style.forms} onSubmit={handleSubmit(submitHandler)}>
                 <label className={style.labels} htmlFor="email">Nombre de usuario</label>
-                <input className={style.inputs} name="email" defaultValue="" id={style.inputlog} placeholder='example@email.com' {...register("email")}/>
+                <input className={style.inputs} name="email" defaultValue="" id={style.inputlog} placeholder='example@email.com' {...register("email")} />
                 <label className={style.labels} htmlFor="pass">Contraseña</label>
-              <label htmlFor="recover" id={style.recover}>Recuperar contraseña</label>
+                <label htmlFor="recover" id={style.recover}>Recuperar contraseña</label>
                 <input className={style.inputs} name='pass' type='password' defaultValue="" id={style.inputlog} placeholder={'**********'} {...register("pass", { required: true })} />
                 {errors.pass && <span>Inserte la contraseña</span>}
                 <input type="submit" value="Ingresar" className={style.sendButton} />
@@ -74,7 +74,7 @@ const Login = (props) => {
             </Col>
           </Row>
         </Container>
-      <img src={wheel} alt="wheel" className={style.wheel}/>
+        <img src={wheel} alt="wheel" className={style.wheel} />
       </div>
     </>
   )
