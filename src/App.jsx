3762045b22e99f57107d19da8f3260bfc4381558
Gerald <aligerald.cac7@gmail.com>
2023-03-16 17:06:32 -0500

    Update styles component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/GlobalStyles.css';
-import NewParther from './Views/NewParther'
+import NewParther from './views/ViewNewParther'
+
 
 function App() {
   return (
---
+++ src/views/ViewNewParther.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import NewParther from '../components/NewPartner/NewParther';
+import Footer from '../components/Footer/Footer';
+
+const ViewNewParther = () => {
+  return (
+    <div>
+      <Header />
+      <NewParther />
+      <Footer />
+    </div>
+  )
+}
+
+export default ViewNewParther
