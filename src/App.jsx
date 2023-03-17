82a3639542ca1409f09ea72c865c638c25952008
grisel1998 <griselpulvirenti@gmail.com>
2023-03-17 08:52:14 -0500

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/GlobalStyles.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-
+import PatnerTodo from './components/PatnerTodo'
 function App() {
   return (
     <div className="App">
       <Header />
+      <PatnerTodo />
       <Footer />
     </div>
   )
